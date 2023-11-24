971e928075a86688ef9a66469c24e3b2af8a90f1
fedenova <federiconovaresee@gmail.com>
2023-11-24 10:53:29 +0100

    update

--- Random i C#/Program.cs
+++ Random i C#/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-!!!!
+﻿using System;
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a person's name:");
+        string personNavn = Console.ReadLine();
+
+        string randomHobby = GetRandomHobby();
+
+        Console.WriteLine($"{personNavn}'s random hobby is: {randomHobby}");
+
+        Console.ReadLine(); 
+    }
+
+    static string GetRandomHobby()
+    {
+        string[] hobbies = { "Running", "Music", "Cooking", "Painting" };
+
+        Random random = new Random();
+        int index = random.Next(hobbies.Length);
+
+        return hobbies[index];
+    }
+    
+
+
+        
+
+}
+
+
+
+
